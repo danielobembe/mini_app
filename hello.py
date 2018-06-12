629640f98c5bf70614e874907038bdb60cf6d001
Daniel Obembe <ayo.obembe@gmail.com>
2018-06-12 10:12:48 -0400

    examples of canonical URL's i.e for projects, and about

--- hello.py
+++ hello.py
@@ -14,20 +14,33 @@ def hello_world():
 @app.route('/user/<username>')
 def show_user_profile(username):
     #show the user-profile for that user
-    return 'User %s' % username
+    return 'User: %s' % username
 
 @app.route('/post/<int:post_id>')
 def show_post_(post_id):
     #show post with given integer id
-    return 'Post %d' % post_id
+    return 'Post: %d' % post_id
 
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
     #show subpath after path
-    return 'Subpath %s' % subpath
+    return 'Subpath: %s' % subpath
 
 """note: converter types: string, int, float
                             path, uuid   """
+
+#Unique/Canonical URLs
+@app.route('/projects/')
+def projects():
+    #note trailing slash
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    #not no trailing slash
+    return 'The about page'
+
+
 
 
 
