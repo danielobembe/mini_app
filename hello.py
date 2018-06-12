000a9d86e87db228889bcb6a86d5af21a13a6e60
Daniel Obembe <ayo.obembe@gmail.com>
2018-06-12 10:07:13 -0400

    included examples of variable routing rules

--- hello.py
+++ hello.py
@@ -1,11 +1,33 @@
 from flask import Flask 
 app = Flask(__name__)
 
-@app.route("/")
+#Basic routing
+@app.route('/')
 def index():
     return "Index page"
 
-@app.route("/hello")
+@app.route('/hello')
 def hello_world():
     return "Hello, World!"
+
+#Routing with variable rules
+@app.route('/user/<username>')
+def show_user_profile(username):
+    #show the user-profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post_(post_id):
+    #show post with given integer id
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    #show subpath after path
+    return 'Subpath %s' % subpath
+
+"""note: converter types: string, int, float
+                            path, uuid   """
+
+
 
